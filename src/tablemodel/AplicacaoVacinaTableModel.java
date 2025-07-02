2ff424e8b7d40404b5b994129e5c77e3e89c8b40
Henriq <henriqueac00.gmail.com>
2025-07-02 16:32:39 -0300

    tablemodel: Adiciona VacinaTableModel e AplicacaoVacinaTableModel

---
+++ src/tablemodel/AplicacaoVacinaTableModel.java
@@ -0,0 +1,61 @@
+package tablemodel;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.table.AbstractTableModel;
+import model.bean.AplicacaoVacina;
+
+public class AplicacaoVacinaTableModel extends AbstractTableModel {
+    
+    private List<AplicacaoVacina> listaAplicacaoVacina;
+    private String[] colunas = {"Paciente", "Responsavel", "Vacina", "Data de Aplicacao", "Local de Aplicacao"};
+    
+    public AplicacaoVacinaTableModel(){
+        listaAplicacaoVacina = new ArrayList<>();
+        
+    }
+    
+    public AplicacaoVacinaTableModel(List<AplicacaoVacina> aplicacaoVacina){
+        this();
+        this.listaAplicacaoVacina.addAll(aplicacaoVacina);
+    }
+    
+    @Override
+    public int getRowCount(){
+        return listaAplicacaoVacina.size();
+    }
+    
+    @Override
+    public int getColumnCount(){
+        return colunas.length;
+    }
+    
+    @Override
+    public Object getValueAt(int linha, int coluna){
+        AplicacaoVacina aplicacaoVacina = listaAplicacaoVacina.get(linha);
+        switch(coluna){
+            case 0:
+                return aplicacaoVacina.getPaciente().getNome();
+            case 1:
+                return aplicacaoVacina.getResponsavel().getNome();
+            case 2:
+                return aplicacaoVacina.getDataAplicacao();
+            case 3:
+                return aplicacaoVacina.getLocalAplicacao();
+            default:
+                return "";
+        }
+    }
+    
+    @Override
+    public String getColumnName(int column){
+        return colunas[column];
+    }
+    
+    public AplicacaoVacina getAplicacaoVacina(int linha){
+        if (linha >= listaAplicacaoVacina.size()){
+            return null;
+        }
+        return listaAplicacaoVacina.get(linha);
+    }
+}
---
+++ src/tablemodel/VacinaTableModel.java
@@ -0,0 +1,58 @@
+package tablemodel;
+
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import model.bean.Vacina;
+
+public class VacinaTableModel extends AbstractTableModel {
+    
+    private List<Vacina> listaVacinas;
+    private String[] colunas = {"Nome", "Fabricante", "Doses Necessarias"};
+    
+    public VacinaTableModel(){
+        listaVacinas = new ArrayList<>();
+    }
+    
+    public VacinaTableModel(List<Vacina> vacinas){
+        this();
+        this.listaVacinas.addAll(vacinas);
+    }
+    
+    @Override
+    public int getRowCount(){
+        return listaVacinas.size();
+    }
+    
+    @Override
+    public int getColumnCount(){
+        return colunas.length;
+    }
+    
+    @Override
+    public Object getValueAt(int linha, int coluna){
+        Vacina vacina = listaVacinas.get(linha);
+        switch(coluna){
+            case 0:
+                return vacina.getNome();
+            case 1:
+                return vacina.getFabricante();
+            case 2:
+                return vacina.getDosesNecessarias();
+            default:
+                return "";
+        }
+    }
+    
+    @Override
+    public String getColumnName(int column){
+        return colunas[column];
+    }
+    
+    public Vacina getVacina(int linha){
+        if (linha >= listaVacinas.size()){
+            return null;
+        }
+        return listaVacinas.get(linha);
+    }
+}
